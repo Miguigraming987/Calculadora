bbd01cc72b5e011aff8e853b211aec308a10d8c8
Miguigraming987 <magogayar@gmail.com>
2024-12-17 21:07:19 +0100

    Creado operador division

--- Calculadora1.java
+++ Calculadora1.java
@@ -2,7 +2,20 @@ public class Calculadora1 {
     public static void main(String[] args) {
     // Valores predefinidos (puedes cambiarlos aquí)
     double num1 = 10.5;
-    char operador = '+';
-    double num2 = 3.2;
+char operador = '/';
+double num2 = 3.2;
+double resultado = 0;
+switch (operador) {
+case '/':
+if (num2 == 0) {
+System.out.println("Error: División entre cero");
+} else {
+resultado = num1 / num2;
+}
+break;
+default:
+System.out.println("Operador inválido");
+return;
     }
+}
 }
